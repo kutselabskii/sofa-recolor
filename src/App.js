86ab830a802d3d0c166d39b6b20133075942bb39
Egor Kutselabskii <osutanfirst@gmail.com>
2021-06-16 23:33:11 +1000

    Attempt to use Fast SCNN model (seems like relu is not supported)

--- src/App.js
+++ src/App.js
@@ -2,26 +2,41 @@ import './App.css';
 import * as tf from '@tensorflow/tfjs';
 import React from 'react';
 
-class BatchNorm extends tf.layers.Layer {
-  static className = 'BatchNorm';
+// class ResizeLayer(tf.keras.layers.Layer):
+//     def __init__(self, w, h, name=None, **kwargs):
+//         super(ResizeLayer, self).__init__()
 
-  // constructor(config) {
-	// 	super(config);
-	// }
+//         self.w = w
+//         self.h = h
 
-  call(x) {
-    return tf.batchNorm(x);
+//     def call(self, inputs):
+//         return tf.image.resize(inputs, (self.w, self.h))
+
+//     def get_config(self):
+//         config = super().get_config().copy()
+//         config.update({
+//             'w': self.w,
+//             'h': self.h,
+//         })
+//         return config
+
+class ResizeLayer extends tf.layers.Layer {
+  static className = 'ResizeLayer';
+
+  constructor(config) {
+		super(config);
+    this.w = config.w;
+    this.h = config.h;
 	}
 
-  // static get className() {
-	// 	console.log(className);
-	//   return className;
-	// }
+  call(x) {
+    return tf.image.resizeBilinear(x, [this.w, this.h]);
+	}
 }
 
 class App extends React.Component {
   async componentDidMount() {
-    tf.serialization.registerClass(BatchNorm);
+    tf.serialization.registerClass(ResizeLayer);
     const model = await tf.loadLayersModel('tfjs_model/model.json');
   }
 
