81855185697684d42e94f6fb2b33551e0e29da09
Egor Kutselabskii <osutanfirst@gmail.com>
2021-06-18 03:42:45 +1000

    Add model that might work, need further work to make it use webcam data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import * as tf from '@tensorflow/tfjs';
-import React from 'react';
+import React, { useRef } from 'react';
 
 // class ResizeLayer(tf.keras.layers.Layer):
 //     def __init__(self, w, h, name=None, **kwargs):
@@ -32,15 +32,74 @@ class ResizeLayer extends tf.layers.Layer {
   call(x) {
     return tf.image.resizeBilinear(x, [this.w, this.h]);
 	}
+
+  computeOutputShape(input_shape) {
+    return [input_shape[0], this.w, this.h, input_shape[3]]
+  }
 }
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+    this.photoRef = React.createRef();
+
+    this.state = {
+      webcam: null
+    };
+  }
+
   async componentDidMount() {
     tf.serialization.registerClass(ResizeLayer);
-    const model = await tf.loadLayersModel('tfjs_model/model.json');
+    this.model = await tf.loadLayersModel('fast_scnn_model/model.json');
+
+    this.getVideo();
+  }
+
+  getVideo() {
+    navigator.mediaDevices
+      .getUserMedia({ video: { width: 300 } })
+      .then(async function(stream) {
+        let video = this.videoRef.current;
+        video.srcObject = stream;
+
+        video.play();
+
+        let cam = await tf.data.webcam(video, {
+                resizeWidth: 196,
+                resizeHeight: 196,
+        });
+
+        this.setState()
+      })
+      .catch(err => {
+        console.error("error:", err);
+      });
+  }
+
+  predict() {
+    
   }
 
   render() {
+    // useEffect(() => {
+    //   getVideo();
+    // }, [videoRef]);
+
+    const getVideo = () => {
+      navigator.mediaDevices
+        .getUserMedia({ video: { width: 300 } })
+        .then(stream => {
+          let video = this.videoRef.current;
+          video.srcObject = stream;
+          video.addEventListener('loadeddata', this.predict);
+          video.play();
+        })
+        .catch(err => {
+          console.error("error:", err);
+        });
+    };
+
     return (
       <div className="App">
         <header className="App-header">
@@ -51,6 +110,7 @@ class App extends React.Component {
             <li>Observe results</li>
           </ol>
           <button>Enable camera</button>
+          <video ref={this.videoRef}/>
         </header>
       </div>
     );
